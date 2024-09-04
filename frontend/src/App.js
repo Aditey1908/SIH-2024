3ad5c16f1f1b585129f6eeb10f209c6715f5f515
Aditey1908 <aditeyrk19@gmail.com>
2024-09-05 00:05:37 +0530

    Loginbttn-redirection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import HelloWorld from './components/Helloworld';
 import Login from './components/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/hello-world" element={<HelloWorld />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
-
